2f40f9e4327e3d39c4ccc63190c08a3b0274371b
Alex C <calex781@gmail.com>
2024-01-24 12:58:35 -0500

    Implement api that returns location information from zip code.
    - Import and configure dot-env for react native
    - Fetch API when zipCode state is changed
    - Add .env to ignore
    - Import and implement useEffect to make API call when zipCode changes

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, SafeAreaView } from "react-native";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { ZIP_KEY } from "@env";
 // Import components
 
 import CurrentTemp from "./components/CurrentTemp";
@@ -10,6 +11,16 @@ import Navbar from "./components/Navbar";
 import SearchBar from "./components/SearchBar";
 export default function App() {
   const [zipCode, setZipCode] = useState();
+  const geoURL = `https://api.geocod.io/v1.7/geocode?postal_code=${zipCode}&api_key=${ZIP_KEY}`;
+  const fetchZip = async () => {
+    const api = await fetch(geoURL);
+    const data = await api.json();
+    console.log(data);
+  };
+  // fetch Coordinates from zip code when zip code changes
+  useEffect(() => {
+    fetchZip();
+  }, zipCode);
   return (
     <SafeAreaView style={styles.container}>
       <SearchBar setZipCode={setZipCode} />
