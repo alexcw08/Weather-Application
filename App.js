a7e87f9e99e350a682ee38b4a962ffa0bcb93eed
Alex C <calex781@gmail.com>
2024-01-25 19:34:36 -0500

    Add error handling for geocod api.
    - Try catch block added for fetchZip function.
    - Handled error where a "valid" zip code is entered but no results are returned.
    - Return results array instead of set array in fetchZip function.
    - Implement Alerts whenever one of the above errors are encountered.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, SafeAreaView } from "react-native";
+import { StyleSheet, Text, SafeAreaView, Alert } from "react-native";
 import { useState, useEffect } from "react";
 import { ZIP_KEY } from "@env";
 // Import components
@@ -22,16 +22,31 @@ export default function App() {
 
   const fetchZip = async () => {
     if (zipCode != null) {
-      const api = await fetch(geoURL);
-      const data = await api.json();
-      const locationData = data.results[0];
-      setLocation({
-        city: locationData.address_components.city,
-        state: locationData.address_components.state,
-        lat: locationData.location.lat,
-        long: locationData.location.lng,
-        zipCode: zipCode,
-      });
+      try {
+        const res = await fetch(geoURL);
+        const data = await res.json();
+        // api responds with 200 OK even wehn no results for given zip
+        if (data.results.length == 0) {
+          Alert.alert("Error", `No results found for ZIP Code ${zipCode}`);
+        } else {
+          // console.log("returning:", data.results[0]);
+          return data.results[0];
+        }
+      } catch (error) {
+        Alert.alert("Fetch Error", `API Fetch Error: ${error}`);
+      }
+
+      //   } else {
+      //     const locationData = data.results[0];
+      //     setLocation({
+      //       city: locationData.address_components.city,
+      //       state: locationData.address_components.state,
+      //       lat: locationData.location.lat,
+      //       long: locationData.location.lng,
+      //       zipCode: zipCode,
+      //     });
+      //   }
+      // }
     }
   };
 
